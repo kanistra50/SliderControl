e9049bebb9ec7ad3f80405e1ba679230739096f2
Nikolay Naumenko <naumenko.mykola@gmail.com>
2016-10-31 11:11:41 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,29 +1,44 @@
-var accord = document.querySelectorAll(".accord");
-console.log(accord);
+$(document).ready(function(){
+  
+    var $container = $('.container');
+    var $main = $('.main');
+    var $btn = $('.button');
+    var $range = $('input.range');
+    var $back = $('.back');
+    var $img = $('.img');
+  
+    $container
+        .mouseover(function() {
+            $main.removeClass("hide");
+            $back.addClass("shadow");
+        })
+        .mouseout(function() {
+            $main.addClass("hide");
+            $back.removeClass("shadow");
+        });
+ 
+    $img.click(function() {
+            $img.removeClass("plus")
+            .addClass("tick");
+        });
 
-for (var i=0; i < accord.length; i++) {
+    $range.on('input', onChangeRange);
+  
+    
+});
 
-    accord[i].onclick = function () {
-        var ul = this.nextElementSibling; //After click <p> always the next Element will be appropriate element <li>..</li>
-        console.log(ul);
-        var img = this.childNodes[1];     //child tag of <p> on position 1 is <jpg> node.
-        console.log(img);
-        if (ul.classList.contains("show")) {
-            img.classList.remove("rotate");
-            ul.classList.remove("opacity");
-            setTimeout(function () {
-                ul.classList.remove("show");
-                ul.classList.add("hide");
-                } , 150)
-            
-        }
-        else  {
-            ul.classList.remove("hide");
-            img.classList.add("rotate");
-            ul.classList.add("show");
-            setTimeout(function () {
-            ul.classList.add("opacity")} , 150)
-            };
-    }
+function onChangeRange(e) {
+  var text = $(this).attr('data');
+  var value = e.target.value;
+  var $text = $('.input_data[data="'+text+'"]');
+  $text.html('+'+value);
+};
 
+function toogler() {
+    console.log("Yes");
+    if ( $( this ).is( ".plus" ) ) {
+    return "tick";
+  } else {
+    return "plus";
+  }
 };
